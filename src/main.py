031829e42cec318c4ad27f00b69d96fe9279bcdc
Alberto Frontino <95907528+albfront@users.noreply.github.com>
2021-12-10 10:31:19 +0000

    .

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+import math
+k=math.sin(2)
+print(k)
+print("Hello World!")
+radius = 1.0
+area = math.pi*radius**2
+volume = 4.0/3.0*math.pi*radius**3
+print("Area del cerchio di raggio", radius, "=", area)
+print("Volume della sfera di raggio", radius, "=", volume)
